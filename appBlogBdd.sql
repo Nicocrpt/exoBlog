8095563f4da66c129f3750f512959dbab073bfd3
Nicolas Carpita <nicolascarpita@Nicolass-MacBook-Pro.local>
2024-05-02 10:59:59 +0200

    ajout script SQL

--- appBlogBdd.sql
+++ appBlogBdd.sql
@@ -1,3 +1,30 @@
-DROP DATABASE posts IF EXISTS ;
+DROP DATABASE IF EXISTS posts ;
 
 CREATE DATABASE posts CHARACTER SET = 'utf8' COLLATE = 'utf8_general_ci' ;
+
+USE posts ;
+
+CREATE TABLE post(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    date_post DATETIME DEFAULT CURRENT_TIMESTAMP,
+    titre VARCHAR(100) NOT NULL,
+    contenu VARCHAR(400) NOT NULL
+);
+
+CREATE TABLE commentaires(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    date_comment DATE DEFAULT CURRENT_TIMESTAMP,
+    auteur VARCHAR(100) NOT NULL,
+    contenu VARCHAR(200) NOT NULL,
+    id_post INT NOT NULL,
+    FOREIGN KEY (id_post) REFERENCES post(id)
+);
+
+INSERT INTO post (titre, contenu) VALUES
+('SALUT', 'Hello_world');
+
+INSERT INTO commentaires (auteur, contenu, id_post) VALUES
+('Pierre', 'Pas ouf', 1);
+
+SELECT auteur, commentaires.contenu, titre AS post FROM commentaires JOIN post ON commentaires.id_post = post.id ;
+
